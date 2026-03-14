87008d45dcbd0e1232bdca6d15614a1679699670
DePayM-Protocol <decentralisedpayments@gmail.com>
2026-03-14 11:44:17 +0300

    setPrimary Error

--- migrations/setPrimaryWallet.js
+++ migrations/setPrimaryWallet.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('../models/User'); // adjust path if needed
+const User = require('../models/user'); // adjust path if needed
 
 async function migratePrimaryWallet() {
   await mongoose.connect(process.env.MONGO_URI, {
